e2c3f5aec586dd99ff57dbe7d8d753833fce42a3
kurra chandu <nanic6465@gmail.com>
2023-11-03 16:39:45 +0530

    adding quantity tto the structure

--- src/config.js
+++ src/config.js
@@ -4,24 +4,28 @@ export const FoodItems = [
       itemTitle:"Pizza",
       itemDescription:"cheese",
       itemPrice:'200',
+      itemQuantity:1,
     },
     {
       itemImage:"https://lh5.googleusercontent.com/p/AF1QipMkEMgZaIA4-dUIFy2detQa8tVi0iyXuad2x-Pb=w92-h92-n-k-no",
       itemTitle:"Burger",
       itemDescription:"Veg",
       itemPrice:'120',
+      itemQuantity:1,
     },
     {
       itemImage:"https://www.licious.in/blog/wp-content/uploads/2020/12/Penne-Alfredo-600x600.jpg",
       itemTitle:"Pasta",
       itemDescription:"Vegan Food",
       itemPrice:'100',
+      itemQuantity:1,
     },
     {
       itemImage:"https://www.licious.in/blog/wp-content/uploads/2022/06/shutterstock_1264839352.jpg",
       itemTitle:"Salad",
       itemDescription:"Chicken",
       itemPrice:'220',
+      itemQuantity:1,
     }
     ]
      
